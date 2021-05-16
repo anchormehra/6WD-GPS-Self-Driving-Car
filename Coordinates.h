3f89cd6c8de557cd8deed948c4ee0e1347da12fd
Vikash Mehra <82005606+anchormehra@users.noreply.github.com>
2021-05-15 18:09:38 -0700

    Create Coordinates.h

---
+++ Coordinates.h
@@ -0,0 +1,13 @@
+int wayPointsSize=10; // number of waypoints, should be the same with the first dimension of wayPoints[thisGuy][2];
+double wayPoints[10][2]={ 
+{33.676765, -117.913750},//GPS Coordinates in latitude, longitude
+{33.676745, -117.913832},
+{33.676735, -117.914043},
+{33.676740, -117.913858},
+{33.676738, -117.913651},
+{33.676738, -117.913507},
+{33.676647, -117.913498},
+{33.676542, -117.913498},
+{33.676661, -117.913503},
+{33.676745, -117.913513}
+};
